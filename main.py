c759e11570000c6e5c250b5df4238bbc6ec48c1a
SanketMali27 <sanketsmali27@gmail.com>
2026-05-30 14:20:32 +0530

    Initial project upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+
+from langchain_groq import ChatGroq
+
+
+load_dotenv()
+
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile"
+)
+
+response = llm.invoke(
+    "Explain Langchain in 3 lines."
+)
+
+print(response.content)
